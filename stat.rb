7f53e8c03dbf4b6b24aea572a9970fec2d6df5e2
Ilya Nikitenkov <me@nikitenich.ru>
2023-06-19 23:06:48 +0300

    Stat: desceding output sort

--- stat.rb
+++ stat.rb
@@ -2,9 +2,10 @@ require 'json'
 
 class Array
   def values_count_by_key(key)
-    each_with_object({}) do |val, sum|
+    result = each_with_object({}) do |val, sum|
       sum[val[key]] = sum[val[key]].to_i + 1
     end
+    result.sort_by { |k, v| -v }.to_h
   end
 end
 
@@ -19,5 +20,5 @@ transferred_phones_stat = transferred_phones.values_count_by_key('current_provid
 mostly_abandoned_stat = transferred_phones.values_count_by_key('previous_provider')
 
 puts "Количество переходных номеров: #{transferred_phones.count}/#{contacts.map { |c| c['phones'] }.flatten.count}"
-puts "Покидаемость: #{mostly_abandoned_stat}"
-puts "Переходность: #{transferred_phones_stat}"
+puts "Покидаемость (количество уходов от операторов): #{mostly_abandoned_stat}"
+puts "Переходность (количество приходов к операторам): #{transferred_phones_stat}"
