13f2ded853f796e72f5a1b1bfa3ecc4f44d1b8b3
Ilya Nikitenkov <me@nikitenich.ru>
2023-06-19 23:50:41 +0300

    Stat: подсчёт количества переходов от тех или иных операторов к другим

--- stat.rb
+++ stat.rb
@@ -1,11 +1,14 @@
 require 'json'
 
+class Hash
+  def sort_desc_by_value
+    sort_by { |_k, v| -v }.to_h
+  end
+end
+
 class Array
   def values_count_by_key(key)
-    result = each_with_object({}) do |val, sum|
-      sum[val[key]] = sum[val[key]].to_i + 1
-    end
-    result.sort_by { |k, v| -v }.to_h
+    each_with_object({}) { |val, sum| sum[val[key]] = sum[val[key]].to_i + 1 }.sort_desc_by_value
   end
 end
 
@@ -20,5 +23,16 @@ transferred_phones_stat = transferred_phones.values_count_by_key('current_provid
 mostly_abandoned_stat = transferred_phones.values_count_by_key('previous_provider')
 
 puts "Количество переходных номеров: #{transferred_phones.count}/#{contacts.map { |c| c['phones'] }.flatten.count}"
-puts "Покидаемость (количество уходов от операторов): #{mostly_abandoned_stat}"
-puts "Переходность (количество приходов к операторам): #{transferred_phones_stat}"
+puts "Покидаемость (количество уходов от операторов): #{JSON.pretty_generate(mostly_abandoned_stat)}"
+puts "Переходность (количество приходов к операторам): #{JSON.pretty_generate(transferred_phones_stat)}"
+
+# {'название оператора, от которого уходят' -> [операторы, к которым ушли]}
+abadoned_to_transfered_stat = transferred_phones.each_with_object({}) do |phone, hash|
+  hash[phone['previous_provider']] ||= []
+  hash[phone['previous_provider']] << phone['current_provider']
+end
+# а теперь считаем операторов, к которым ушли 
+abadoned_to_transfered_stat.transform_values! do |value|
+  value.each_with_object(Hash.new(0)) { |carrier, hash| hash[carrier] += 1 }.sort_desc_by_value
+end
+puts "Статистика, к кому и как часто уходят от операторов (от кого -> количество тех, к кому): #{JSON.pretty_generate(abadoned_to_transfered_stat)}"
