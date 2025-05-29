76953c2b76ba8b99b7a60f17f2b080f207201538
Ilya Nikitenkov <me@nikitenich.ru>
2025-05-29 22:43:31 +0300

    remove unused code

--- parse.rb
+++ parse.rb
@@ -10,17 +10,8 @@ require 'uri'
 
 SUPPORTED_CONTACT_FIELDS = %i[first_name last_name middle_name phones]
 
-Phone = Struct.new(:number, :current_provider, :previous_provider, keyword_init: true) do
-  def to_s
-    "#{number}: #{previous_provider == '' ? current_provider : "#{previous_provider} -> #{current_provider}"}"
-  end
-end
-
-Contact = Struct.new(*SUPPORTED_CONTACT_FIELDS, keyword_init: true) do
-  def to_s
-    "#{first_name} #{last_name}: #{phones.map(&:to_s).join("\n")}"
-  end
-end
+Phone = Struct.new(:number, :current_provider, :previous_provider, keyword_init: true)
+Contact = Struct.new(*SUPPORTED_CONTACT_FIELDS, keyword_init: true)
 
 csv = SmarterCSV.process('contacts.csv')
 contacts = csv&.select! { it.keys.map(&:to_s).any? { it.include?('name') } }
@@ -29,9 +20,9 @@ contacts = csv&.select! { it.keys.map(&:to_s).any? { it.include?('name') } }
 
   filtered_phones = contact.slice(*phone_keys)
                            .values
-                           .flat_map { it.to_s.split(':::') } # sometimes multiple phones presents at the same key
-                           .map { it.scan(/\d/).join } # remain only digits
-                           .select { it.start_with?('79') } # remove non-Russian phones
+                           .flat_map { it.to_s.split(':::') }  # sometimes multiple phones presents at the same key
+                           .map { it.scan(/\d/).join }         # remain only digits
+                           .select { it.start_with?('79') }    # remove non-Russian phones
                            .map { Phone.new(number: it.to_i) } # create an object
   contact.merge!(phones: filtered_phones)
   contact.slice!(*SUPPORTED_CONTACT_FIELDS)
@@ -41,11 +32,11 @@ end
 # removing contacts without valid phones
 contacts&.reject! { |contact| contact.phones.empty? }
 
-contacts&.each_with_index do |contact, i|
-  puts "(#{i + 1}/#{contacts&.size}) Checking contact #{contact.inspect}..."
+contacts&.each_with_index do |contact, index|
+  puts "(#{index + 1}/#{contacts&.size}) Checking contact #{contact.inspect}..."
   contact.phones.each do |phone|
     puts "Checking phone #{phone.number}..."
-    initial_provider, moved_to_operator = Faraday.post('https://www.kody.su/embed/widget.php') do |req|
+    initial_provider, moved_to_provider = Faraday.post('https://www.kody.su/embed/widget.php') do |req|
       req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
       req.body = URI.encode_www_form({ number: phone.number })
     end.then { Nokogiri::HTML(it.body) }
@@ -58,9 +49,9 @@ contacts&.each_with_index do |contact, i|
       end
     end
     puts "\tInitial: #{initial_provider}"
-    puts "\tMoved To: #{moved_to_operator}"
-    if moved_to_operator
-      phone.current_provider = moved_to_operator
+    puts "\tMoved To: #{moved_to_provider}"
+    if moved_to_provider
+      phone.current_provider = moved_to_provider
       phone.previous_provider = initial_provider
     else
       phone.current_provider = initial_provider
